6a96388ef81ce79b54501250e308e66bb0e8b1a8
fellodurant <fellodurant97@gmail.com>
2019-02-16 13:37:59 +0300

    Created a basic Java startpoint: App.javaL:

--- src/main/java/com/felix/pets/App.java
+++ src/main/java/com/felix/pets/App.java
@@ -1,11 +1,6 @@
 package com.felix.pets;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
     }
 }
