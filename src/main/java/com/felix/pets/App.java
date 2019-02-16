704b19a2b8568e556d09c474aa10ff8b91a3c156
fellodurant <fellodurant97@gmail.com>
2019-02-16 13:53:47 +0300

    Created the basic javafx.application.Application class

--- src/main/java/com/felix/pets/App.java
+++ src/main/java/com/felix/pets/App.java
@@ -1,6 +1,16 @@
 package com.felix.pets;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
     public static void main(String[] args) {
+        Application.launch(args);
     }
 }
