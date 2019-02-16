c9bc01846116036020cadd89174b0a22aaf0ee57
fellodurant <fellodurant97@gmail.com>
2019-02-16 13:56:58 +0300

    Created the base user interface

--- src/main/java/com/felix/pets/App.java
+++ src/main/java/com/felix/pets/App.java
@@ -1,13 +1,18 @@
 package com.felix.pets;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        BorderPane root = new BorderPane(new Label("Start"));
+        stage.setScene(new Scene(root, 400, 30));
+        stage.show();
     }
 
     public static void main(String[] args) {
